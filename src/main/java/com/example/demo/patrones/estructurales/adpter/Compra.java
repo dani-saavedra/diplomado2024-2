b1d5cfb4caae49348ba9f96faa1e5bfdae16daaf
dani-saavedra <daniel.saavedra.fon@gmail.com>
2024-07-18 20:16:37 -0500

    Ejemplo sin adapter

--- src/main/java/com/example/demo/patrones/estructurales/adpter/Compra.java
+++ src/main/java/com/example/demo/patrones/estructurales/adpter/Compra.java
@@ -15,4 +15,15 @@ public class Compra {
         //LOGICA PARA CONFIRMAR LA COMPRA
         sistemaPago.procesarPago(monto);
     }
+
+    public void confirmarCompraMal(String cliente, int monto, List<String> productos, int proveedor) {
+        if (proveedor == 1) {
+            PayPal payPal = new PayPal();
+            payPal.procesarPago(monto, "102030");
+        } else if (proveedor == 2) {
+            SistemaPagoPropio sistemaPagoPropio = new SistemaPagoPropio();
+            sistemaPagoPropio.procesarPago(monto);
+        }
+
+    }
 }
