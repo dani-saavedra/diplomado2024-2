3767d78007f70f1d15f0aa8aa4adba0ec3b01bd7
dani-saavedra <daniel.saavedra.fon@gmail.com>
2024-08-12 18:49:13 -0500

    Otro commit de prueba

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication
+public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("prueba");
         SpringApplication.run(DemoApplication.class, args);
     }
 
